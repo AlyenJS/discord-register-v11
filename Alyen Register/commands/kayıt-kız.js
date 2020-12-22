0d7c155358384cfd58f89ed5a9076cf3df20292c
Hasan <64003407+AlyenJS@users.noreply.github.com>
2020-12-22 19:30:06 +0300

    Update kayıt-kız.js

--- Alyen Register/commands/kayıt-kız.js
+++ Alyen Register/commands/kayıt-kız.js
@@ -1,15 +1,15 @@
+
 const Discord = require('discord.js');
 const db = require('quick.db')
 module.exports.run = async (bot, message, args, member, client, level) => {
-  const alyenemoji = bot.emojis.find(emoji => emoji.name === "emojiismi");
+  const alyenemoji = bot.emojis.find(emoji => emoji.name === "tepki olarak verilcke emoji");
   if (!message.member.hasPermission("ADMINISTRATOR"))
   if (!message.member.hasPermission("MANAGE_ROLES"))
-  if (!message.member.roles.find('name', '#414 | Kayıt')) return message.channel.send('Yetkin yetmiyor.');
+  if (!message.member.roles.find('name', 'Alyen')) return message.channel.send('Yetkin yetmiyor.');
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!user) return message.reply("**Etiket Atmayı Unuttun!**");
-  
-  user.addRole('') ///verilicek rol
-  user.removeRole('')  //alınıcak kayıtsız rolü
+  user.addRole('')  ///verilicek rol
+  user.removeRole('')   //alınıcak rol
 const alyen = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
         .setDescription(`${user}, **Adlı üyenin __kayıt__ işlemi başarılı bir şekilde yapıldı.**`)
